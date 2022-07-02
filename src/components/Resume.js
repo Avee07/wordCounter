90a40960c8d1dc649b414faca6d2a445394c7397
Avee07 <96907238+Avee07@users.noreply.github.com>
2022-07-02 19:57:57 +0530

    Delete Resume.js

--- src/components/Resume.js
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-class Resume extends Component{
-    render(){
-        const { name } = this.props;
-        return <h2>This is a Class Component {name}</h2>
-    }
-
-}
-export default Resume;
