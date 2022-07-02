6523033f68c1e4fbc4c92c9349a66fb382d72647
Avee07 <96907238+Avee07@users.noreply.github.com>
2022-07-02 19:57:15 +0530

    Delete ClassBasedComponent.js

--- src/components/ClassBasedComponent.js
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-class ClassBasedComponent extends Component{
-    render() {
-        return<h1>Yohoho Yohohohohooooooo<br/>Watashi Brook! Yohohohohooooooo<br/>{this.props.messagecontent}<br/>{this.props.code}</h1>
-    }
-}
-
-export default ClassBasedComponent;
